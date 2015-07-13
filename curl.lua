76372f62458f20471004e3f1a3a713b8f00908f2
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-07-13 02:25:32 +0200

    Use autotools project.

--- curl.lua
+++ curl.lua
@@ -12,30 +12,15 @@ local M = {}
 -- @param args.compiler The compiler to use
 -- @param[opt] args.kind 'shared' or 'static' (defaults to 'static')
 function M.build(args)
-	local curl = require("configure.external").Project:new{
-		name = 'cURL',
-		build = args.build,
-	}:download_tarball{
+	local curl = require("configure.external").AutotoolsProject:new(
+		table.update({name = 'cURL'}, args)
+	):download_tarball{
 		url = 'http://curl.haxx.se/download/curl-'.. args.version .. '.tar.gz',
+	}:configure{
+	}:build{
+	}:install{
 	}
 	local kind = args.kind or 'static'
-
-	curl:configure{
-		command = {
-			curl:step_directory('source') / 'configure',
-			'--prefix', tostring(curl:step_directory('install'))
-		},
-		working_directory = curl:step_directory('build'),
-		env = {CC = compiler.binary_path}
-	}:build{
-		command = {
-			args.build:fs():which("make"), "-C", curl:step_directory('build'),
-		}
-	}:install{
-		command = {
-			args.build:fs():which("make"), "-C", curl:step_directory('build'), 'install'
-		}
-	}
 	if kind == 'static' then
 		local lib = curl:node{path = 'lib/libcurl.a'}
 	else
