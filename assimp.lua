623090bec17f0a3514ca427f68faafdb532f18d8
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-09-26 16:05:57 +0000

    assimp: Use the correct source directory.

--- assimp.lua
+++ assimp.lua
@@ -20,7 +20,7 @@ function M.build(args)
 			{
 				name = 'Assimp',
 				kind = kind,
-				source_directory = 'source/assimp-' .. args.version,
+				source_directory = 'source/assimp-' .. args.version .. '/assimp-' .. args.version,
 			},
 			args
 		)
