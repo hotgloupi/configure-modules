023af509bea66dd2af2e555453cce4b3be8f742d
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-09-27 12:12:21 +0000

    librocket: Finish implementation.

--- librocket.lua
+++ librocket.lua
@@ -26,7 +26,8 @@ function M.build(args)
 		table.update({
 			name = 'libRocket',
 			kind = kind,
-			source_directory = 'source/Build'
+			source_directory = 'source/Build',
+			extract_directory = 'source',
 		}, args)
 	):download{
 		url = url,
@@ -36,14 +37,40 @@ function M.build(args)
     if args.c_compiler ~= nil then
 	    configure_variables['CMAKE_C_COMPILER'] = args.c_compiler.binary
 	end
-	if args.freetype2 ~= nil then
-		configure_variables['FREETYPE_INCLUDE_DIRS'] = args.freetype2.include_directories[1]
-		configure_variables['FREETYPE_LIBRARY'] = args.freetype2.files[1]
+	if args.freetype2 == nil then
+		error("You must provide a freetype2 library to build libRocket")
 	end
+	configure_variables['FREETYPE_INCLUDE_DIRS'] = args.freetype2.include_directories[1]
+	configure_variables['FREETYPE_LIBRARY'] = args.freetype2.files[1]
 	configure_variables['BUILD_SHARED_LIBS'] = kind == 'shared'
 	configure_variables['CMAKE_INSTALL_LIBDIR'] = 'lib' -- Prevent arch subdirectory
 	project:configure{variables = configure_variables}
 	project:build{}:install{}
+
+	local libraries = {}
+	for _, lib in ipairs({'RocketCore', 'RocketControls', 'RocketDebugger'}) do
+		local prefix = ''
+		local ext = nil
+		if args.build:target():os() == Platform.OS.windows then
+			ext = '.lib'
+		else
+			prefix = 'lib'
+			if kind == 'static' then
+				ext = '.a'
+			else
+				ext = '.so'
+			end
+		end
+		table.append(libraries, project:node{path = 'lib/' .. prefix .. lib .. ext})
+	end
+	return args.compiler.Library:new{
+		name = project.name,
+		include_directories = {
+			project:directory_node{path = 'include'}
+		},
+		files = libraries,
+		kind = kind,
+	}
 end
 
 return M
