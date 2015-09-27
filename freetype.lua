9e7a1d7b81bd26cb4aa06bc2a492163de3f1c321
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-09-27 12:11:58 +0000

    freetype: Use the cmakefile instead of manually compiling.

--- freetype.lua
+++ freetype.lua
@@ -28,84 +28,36 @@ function M.build(args)
 	):download{
 		url = url,
 	}
-	local filenames = {
-		"src/base/ftsystem.c",
-		"src/base/ftinit.c",
-		"src/base/ftdebug.c",
-		"src/base/ftbase.c",
-		"src/base/ftbbox.c",         -- recommended, see <freetype/ftbbox.h>
-		"src/base/ftglyph.c",        -- recommended, see <freetype/ftglyph.h>
-		"src/base/ftbdf.c",          -- optional, see <freetype/ftbdf.h>
-		"src/base/ftbitmap.c",       -- optional, see <freetype/ftbitmap.h>
-		"src/base/ftcid.c",          -- optional, see <freetype/ftcid.h>
-		"src/base/ftfstype.c",       -- optional
-		"src/base/ftgasp.c",         -- optional, see <freetype/ftgasp.h>
-		"src/base/ftgxval.c",        -- optional, see <freetype/ftgxval.h>
-		"src/base/ftlcdfil.c",       -- optional, see <freetype/ftlcdfil.h>
-		"src/base/ftmm.c",           -- optional, see <freetype/ftmm.h>
-		"src/base/ftotval.c",        -- optional, see <freetype/ftotval.h>
-		"src/base/ftpatent.c",       -- optional
-		"src/base/ftpfr.c",          -- optional, see <freetype/ftpfr.h>
-		"src/base/ftstroke.c",       -- optional, see <freetype/ftstroke.h>
-		"src/base/ftsynth.c",        -- optional, see <freetype/ftsynth.h>
-		"src/base/fttype1.c",        -- optional, see <freetype/t1tables.h>
-		"src/base/ftwinfnt.c",       -- optional, see <freetype/ftwinfnt.h>
-		"src/base/ftxf86.c",         -- optional, see <freetype/ftxf86.h>
-
-		"src/bdf/bdf.c",             -- BDF font driver
-		"src/cff/cff.c",             -- CFF/OpenType font driver
-		"src/cid/type1cid.c",        -- Type 1 CID-keyed font driver
-		"src/pcf/pcf.c",             -- PCF font driver
-		"src/pfr/pfr.c",             -- PFR/TrueDoc font driver
-		"src/sfnt/sfnt.c",           -- SFNT files support (TrueType & OpenType)
-		"src/truetype/truetype.c",   -- TrueType font driver
-		"src/type1/type1.c",         -- Type 1 font driver
-		"src/type42/type42.c",       -- Type 42 font driver
-		"src/winfonts/winfnt.c",     -- Windows FONT / FNT font driver
-
-		"src/raster/raster.c",       -- monochrome rasterizer
-		"src/smooth/smooth.c",       -- anti-aliasing rasterizer
-
-		"src/autofit/autofit.c",     -- auto hinting module
-		"src/cache/ftcache.c",       -- cache sub-system (in beta)
-		"src/gzip/ftgzip.c",         -- support for compressed fonts (.gz)
-		"src/lzw/ftlzw.c",           -- support for compressed fonts (.Z)
-		"src/bzip2/ftbzip2.c",       -- support for compressed fonts (.bz2)
-		"src/gxvalid/gxvalid.c",     -- TrueTypeGX/AAT table validation
-		"src/otvalid/otvalid.c",     -- OpenType table validation
-		"src/psaux/psaux.c",         -- PostScript Type 1 parsing
-		"src/pshinter/pshinter.c",   -- PS hinting module
-		"src/psnames/psnames.c",     -- PostScript glyph names support
+	local configure_variables = {
+		BUILD_SHARED_LIBS = kind ~= 'static',
 	}
 
-	if args.build:target():is_osx() then
-		table.append(
-			filenames,
-			"src/base/ftmac.c"          -- only on the Macintosh
-		)
-	end
+	project:configure{variables = configure_variables}
+	project:build{}:install{}
 
-	local sources = {}
-	for _, filename in ipairs(filenames) do
-		local node = args.build:file_node(
-			project:step_directory('source') / filename
-		)
-		args.build:add_rule(
-			Rule:new():add_source(project:last_step()):add_target(node)
-		)
-		table.append(sources, node)
+	local libraries = {}
+	for _, lib in ipairs({'freetype2'}) do
+		local prefix = ''
+		local ext = nil
+		if args.build:target():os() == Platform.OS.windows then
+			ext = '.lib'
+		else
+			prefix = 'lib'
+			if kind == 'static' then
+				ext = '.a'
+			else
+				ext = '.so'
+			end
+		end
+		table.append(libraries, project:node{path = 'lib/' .. prefix .. lib .. ext})
 	end
-
-	return args.compiler:link_library{
+	return args.compiler.Library:new{
 		name = project.name,
-		sources = sources,
-		kind = args.kind or 'static',
-		object_directory = project:step_directory('build'),
-		directory = project:step_directory('install') / 'lib',
-		defines = {'FT2_BUILD_LIBRARY'},
 		include_directories = {
-			project:step_directory('source') / 'include',
-		}
+			project:directory_node{path = 'include'}
+		},
+		files = libraries,
+		kind = kind,
 	}
 end
 
