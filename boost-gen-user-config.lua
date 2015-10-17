1f2e31da157cdf378edbab328d04a2d3cd1a011f
Raphaël Londeix <raphael.londeix@celoxica.com>
2015-10-17 01:41:03 -0700

    boost: Fix indent in gen script.

--- boost-gen-user-config.lua
+++ boost-gen-user-config.lua
@@ -1,34 +1,49 @@
 
 function main(source_dir, python_exe)
-    source_dir = Path:new(source_dir)
-    if not source_dir:is_absolute() then
-        source_dir = Filesystem:cwd() / source_dir
-    end
+	source_dir = Path:new(source_dir)
+	if not source_dir:is_absolute() then
+		source_dir = Filesystem:cwd() / source_dir
+	end
 
-    python_exe = Path:new(python_exe)
-    if not python_exe:is_absolute() then
-        python_exe = Filesystem:cwd() / python_exe
-    end
+	python_exe = Path:new(python_exe)
+	if not python_exe:is_absolute() then
+		python_exe = Filesystem:cwd() / python_exe
+	end
 
-    local prefix = Process:check_output(
-        {python_exe, '-c', 'import sys; print(sys.prefix)'}
-    ):strip()
+	local prefix = Process:check_output(
+		{python_exe, '-c', 'import sys; print(sys.prefix)'}
+	):strip()
 
-    local include_dir = Process:check_output(
-        {python_exe, '-c', 'import distutils.sysconfig as s; print(s.get_python_inc(True))'}
-    ):strip()
+	local include_dir = Process:check_output(
+		{python_exe, '-c', 'import distutils.sysconfig as s; print(s.get_python_inc(True))'}
+	):strip()
 
-    local version = Process:check_output(
-        {python_exe, '-c', 'import sys; print("%s.%s" % sys.version_info[:2])'}
-    ):strip()
+	local version = Process:check_output(
+		{python_exe, '-c', 'import sys; print("%s.%s" % sys.version_info[:2])'}
+	):strip()
 
-    local library_dir = tostring(Path:new(prefix) / 'lib')
+	local library_dir = Process:check_output(
+		{python_exe, '-c', 'import distutils.sysconfig as s; print(s.get_config_var("LIBDIR"))'}
+	):strip()
 
-    local f = assert(io.open(tostring(source_dir / "user-config.jam") , 'w'))
-    f:write("using python : " .. version .. '\n')
-    f:write("             : " .. tostring(python_exe) .. '\n')
-    f:write("             : " .. include_dir .. '\n')
-    f:write("             : " .. library_dir .. '\n')
-    f:write("             ;\n")
-    f:close()
+	local library = Process:check_output(
+		{python_exe, '-c', 'import distutils.sysconfig as s; print(s.get_config_var("LDLIBRARY"))'}
+	):strip()
+
+	local lib_path = Path:new(library_dir) / library
+	print(lib_path)
+	assert(lib_path:exists())
+	local expected_name = "libpython" .. version .. tostring(lib_path:ext())
+	local expected_path = Path:new(library_dir) / expected_name
+	if not expected_path:exists() then
+		Process:check_output({'cp', lib_path, expected_path})
+	end
+
+	local f = assert(io.open(tostring(source_dir / "user-config.jam") , 'w'))
+	f:write("using python : " .. version .. '\n')
+	f:write("             : " .. tostring(python_exe) .. '\n')
+	f:write("             : " .. include_dir .. '\n')
+	f:write("             : " .. library_dir .. '\n')
+	f:write("             ;\n")
+	f:close()
 end
